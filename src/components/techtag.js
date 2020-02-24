b443f1fe1d1c0d8c96175dd8f2e241ac864869fe
jjungeun <jungeun9729@gmail.com>
2020-02-24 22:35:51 +0900

    tag style

--- src/components/techtag.js
+++ src/components/techtag.js
@@ -9,20 +9,22 @@ const TechTag = props => {
     <div className="d-inline-block p-1">
       <Link to={`/tags/${tag}/`}>
         <button className="tech-tag text-white">
-          <p className="d-inline">{tech} </p>
-          <div className="d-inline" style={{ fontSize: size, color: color }}>
-            <svg
-              role="img"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-              width={size}
-              height={size}
-              style={{ fill: `${color}` }}
-            >
-              <title>{tech}</title>
-              <path d={name} />
-            </svg>
-          </div>
+          <p className="d-inline">
+            <div className="d-inline" style={{ fontSize: size, color: color }}>
+              {tech}
+              <svg
+                role="img"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                width={size}
+                height={size}
+                style={{ fill: `${color}` }}
+              >
+                <title>{tech}</title>
+                <path d={name} />
+              </svg>
+            </div>
+          </p>
         </button>
       </Link>
     </div>
