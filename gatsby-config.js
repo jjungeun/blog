2268b551c0f146758f561f86693279c6214f4c33
jjungeun <jungeun9729@gmail.com>
2020-04-07 19:05:08 +0900

    icon comment

--- gatsby-config.js
+++ gatsby-config.js
@@ -93,7 +93,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `./content/assets/gatsby-icon.png`,
+        // icon: `content/assets/gatsby-icon.png`,
       },
     },
     `gatsby-plugin-netlify`,
