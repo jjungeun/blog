07a1cb78e7afd96aedeb4b25abd8b0b8a2ba2b0b
jjungeun <jungeun9729@gmail.com>
2020-04-10 18:02:24 +0900

    siteconfig edit

--- siteConfig.js
+++ siteConfig.js
@@ -2,7 +2,7 @@ module.exports = {
   name: "JJunglog",
   shortName: "JJunglog",
   description: "Development Log",
-  url: "https://gatsby-london.netlify.com", // Domain of your site without prefix!
+  url: "https://jjunglog.netlify.com/", // Domain of your site without prefix!
   prefix: "/",
   author: "jjungeun",
   github: "https://github.com/jjungeun",
